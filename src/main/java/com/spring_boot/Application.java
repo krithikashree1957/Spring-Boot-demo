08433fd3a3a680a17bdaeb3db269141b4a10713c
Krithika shree K <krithikashree.k2024@vitstudent.ac.in>
2026-07-26 21:03:41 +0530

    Initial heelo world msg in localhost

--- src/main/java/com/spring_boot/Application.java
+++ src/main/java/com/spring_boot/Application.java
@@ -2,12 +2,25 @@ package com.spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+	
+	public String getMethodName(@RequestParam String param) {
+		return new String();
+	}
+	@GetMapping
+	public String helloWorld()
+	{
+		return "Hello World Spring Boot!";
+	}
 }
